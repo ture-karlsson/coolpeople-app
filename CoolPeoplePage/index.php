865d2c3cf53adbb6b513e04e220ac2f872be1ac6
ture-karlsson <tkarlsso@redhat.com>
2016-10-25 14:18:57 +0200

    Update index.php

--- CoolPeoplePage/index.php
+++ CoolPeoplePage/index.php
@@ -28,26 +28,6 @@
           zoom: 6,
           center: {lat: 59.6498, lng: 17.9238}
         });
-        
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
 
 
         // Create an array of alphabetical characters used to label the markers.
