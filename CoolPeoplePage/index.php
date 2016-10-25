8e28498b2706ad59e4251fe51eb20b278ce40196
ture-karlsson <tkarlsso@redhat.com>
2016-10-25 13:56:15 +0200

    Update index.php

--- CoolPeoplePage/index.php
+++ CoolPeoplePage/index.php
@@ -25,8 +25,8 @@
       function initMap() {
 
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: {lat: -28.024, lng: 140.887}
+          zoom: 13,
+          center: {lat: 17.689008899999997, lng: 59.619794399999996}
         });
 
         // Create an array of alphabetical characters used to label the markers.
@@ -48,12 +48,10 @@
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
       }
       var locations = [
-        {lat: -31.563910, lng: 147.154312},
-        {lat: -33.718234, lng: 150.363181},
-        {lat: -33.727111, lng: 150.371124},
-        {lat: -33.848588, lng: 151.209834},
-        {lat: -33.851702, lng: 151.216968},
-        {lat: -34.671264, lng: 150.863657}
+        {lat: 17.689008899999997, lng: 59.619794399999996},
+        {lat: 17.689008899909997, lng: 59.619794399999996},
+        {lat: 17.689008899999997, lng: 59.619794399909996},
+        {lat: 17.689008899909997, lng: 59.619794399909996}
       ]
     </script>
     <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
