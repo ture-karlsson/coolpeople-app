ea1dc83fcb005774c6c35e1376a73d47175c1125
ture-karlsson <tkarlsso@redhat.com>
2016-10-25 14:02:28 +0200

    Update index.php

--- CoolPeoplePage/index.php
+++ CoolPeoplePage/index.php
@@ -25,7 +25,7 @@
       function initMap() {
 
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+          zoom: 8,
           center: {lat: 59.6498, lng: 17.9238}
         });
 
