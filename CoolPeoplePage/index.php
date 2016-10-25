c6d669ed062e6e0b5f675b798a695c074db6c1fc
ture-karlsson <tkarlsso@redhat.com>
2016-10-25 14:16:57 +0200

    Update index.php

--- CoolPeoplePage/index.php
+++ CoolPeoplePage/index.php
@@ -26,8 +26,29 @@
 
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
-          center: {lat: position.coords.latitude, lng: position.coords.longitude}
+          center: {lat: 59.6498, lng: 17.9238}
         });
+        
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
 
         // Create an array of alphabetical characters used to label the markers.
         var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
